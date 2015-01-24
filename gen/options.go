014849ad27c0f69a017e454a15fd3a81c3564929
monochromegane <dev.kuro.obi@gmail.com>
2015-01-24 09:29:16 +0900

    Start rebuild.

--- gen/options.go
+++
@@ -1,6 +0,0 @@
-package gen
-
-type Option struct {
-	Prefix string `short:"p" description:"Add prefix to generated file name." default:""`
-	Suffix string `short:"s" description:"Add suffix to generated file name." default:"_gen"`
-}
--- util.go
+++
@@ -1,7 +0,0 @@
-package ar
-
-import "reflect"
-
-func IsZero(v interface{}) bool {
-	return reflect.ValueOf(v).Interface() == reflect.Zero(reflect.TypeOf(v)).Interface()
-}
