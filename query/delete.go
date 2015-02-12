56666af265eb7c4f02b18899f67fda70ee4e15dc
monochromegane <dev.kuro.obi@gmail.com>
2015-02-13 01:42:10 +0900

    Added delete query builder.

---
+++ query/delete.go
@@ -0,0 +1,36 @@
+package query
+
+import "fmt"
+
+type Delete struct {
+	table string
+	where *condition
+}
+
+func (d *Delete) Table(table string) *Delete {
+	d.table = table
+	return d
+}
+
+func (d *Delete) Where(cond string, args ...interface{}) *Delete {
+	if d.where == nil {
+		d.where = &condition{phrase: "WHERE"}
+	}
+	d.where.addExpression(cond, args...)
+	return d
+}
+
+func (d *Delete) And(cond string, args ...interface{}) *Delete {
+	return d.Where(cond, args...)
+}
+
+func (d *Delete) Build() (string, []interface{}) {
+	binds := []interface{}{}
+	query := fmt.Sprintf("DELETE FROM %s", d.table)
+	if d.where != nil {
+		q, b := d.where.build()
+		query += q
+		binds = append(binds, b...)
+	}
+	return query, binds
+}
---
+++ query/delete_test.go
@@ -0,0 +1,14 @@
+package query
+
+import "testing"
+
+func TestDelete(t *testing.T) {
+	del := Delete{}
+	del.Table("table")
+	del.Where("columnA", "value1")
+
+	q, b := del.Build()
+
+	assertQuery(t, "DELETE FROM table WHERE columnA = ?", q)
+	assertBinds(t, []interface{}{"value1", "value2", "value1"}, b)
+}
