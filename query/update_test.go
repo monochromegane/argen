75e5aa9660a4fa0093064351645877b064363459
monochromegane <dev.kuro.obi@gmail.com>
2015-02-20 20:08:17 +0900

    Fixed broken test.

--- query/update_test.go
+++ query/update_test.go
@@ -7,12 +7,11 @@ func TestUpdate(t *testing.T) {
 	update.Table("table")
 	update.Params(map[string]interface{}{
 		"columnA": "value1",
-		"columnB": "value2",
 	})
 	update.Where("columnA", "value1")
 
 	q, b := update.Build()
 
-	assertQuery(t, "UPDATE table SET columnA = ?, columnB = ? WHERE columnA = ?", q)
-	assertBinds(t, []interface{}{"value1", "value2", "value1"}, b)
+	assertQuery(t, "UPDATE table SET columnA = ? WHERE columnA = ?", q)
+	assertBinds(t, []interface{}{"value1", "value1"}, b)
 }
