dfae49f78a80fa25f070b14083a43bb8d7651801
monochromegane <dev.kuro.obi@gmail.com>
2015-02-20 18:35:17 +0900

    Empty query args isn't now printed in log.

--- logger.go
+++ logger.go
@@ -11,13 +11,20 @@ type Logger struct {
 }
 
 func (l Logger) Print(values ...interface{}) {
+	log := []interface{}{}
 	// currentTime
 	currentTime := "\033[33m[" + time.Now().Format("2006-01-02 15:04:05") + "]\033[0m"
 	// duration
 	duration := fmt.Sprintf(" \033[36;1m[%.2fms]\033[0m ", float64(values[0].(time.Duration).Nanoseconds()/1e4)/100.0)
 	// sql
 	sql := values[1]
-	args := values[2]
 
-	l.Println(currentTime, duration, sql, args)
+	log = append(log, currentTime, duration, sql)
+
+	// args
+	if a, ok := values[2].([]interface{}); ok && len(a) > 0 {
+		log = append(log, a)
+	}
+
+	l.Println(log...)
 }
